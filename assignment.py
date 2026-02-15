298c960f5f0f87b4366ae82712d7b31dd0898856
Anshika Gupta <154658065+Anshi2611@users.noreply.github.com>
2026-02-15 19:31:23 +0530

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -2,10 +2,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# -------------------------
-# STEP 1: Load Dataset
-# -------------------------
-
 df = pd.read_csv("data.csv", encoding="latin1")
 
 
@@ -13,11 +9,6 @@ df = pd.read_csv("data.csv", encoding="latin1")
 x = df["no2"].dropna()
 
 print("Total NO2 values:", len(x))
-
-# -------------------------
-# STEP 2: Enter Your Roll Number
-# -------------------------
-
 r = 102303605
 a_r = 0.05 * (r % 7)
 b_r = 0.3 * ((r % 5) + 1)
@@ -25,15 +16,11 @@ b_r = 0.3 * ((r % 5) + 1)
 print("a_r =", a_r)
 print("b_r =", b_r)
 
-# -------------------------
-# STEP 3: Transform x → z
-# -------------------------
+
 
 z = x + a_r * np.sin(b_r * x)
 
-# -------------------------
-# STEP 4: Estimate Parameters
-# -------------------------
+
 
 mu = np.mean(z)
 variance = np.var(z)
@@ -48,10 +35,6 @@ print("mu =", mu)
 print("lambda =", lambda_val)
 print("c =", c)
 
-# -------------------------
-# STEP 5: Plot Histogram + PDF
-# -------------------------
-
 z_sorted = np.sort(z)
 pdf = c * np.exp(-lambda_val * (z_sorted - mu)**2)
 
@@ -61,9 +44,7 @@ plt.title("Histogram and Learned PDF")
 plt.xlabel("z values")
 plt.ylabel("Density")
 plt.show()
-# -------------------------
-# STEP 6: Save Results to File
-# -------------------------
+
 
 with open("results.txt", "w") as f:
     f.write(f"mu = {mu}\n")
